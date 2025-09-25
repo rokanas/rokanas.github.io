84db540e685fa76bbe28393fce335bb45ad6396b
Konstantinos Rokanas <rokanas@student.chalmers.se>
2025-09-25 21:10:27 +0300

    #25 Fix which social buttons appear by default

--- app/src/components/social_buttons.rs
+++ app/src/components/social_buttons.rs
@@ -32,20 +32,19 @@ pub fn social_buttons(props: &SocialButtonProps) -> Html {
                 </svg>
             </a>
 
+            // linkedin
+            <a 
+                href="https://www.linkedin.com/in/konstantinos-rokanas-1ab1a113a/" 
+                target="_blank" 
+                class={format!("w-{} h-{} bg-[#1a1a1a] hover:bg-blue-600 rounded-lg flex items-center justify-center transition-all duration-300 hover:scale-110 group border-2 border-black hover:border-blue-500", props.button_size, props.button_size)}
+                title="LinkedIn"
+            >
+                <svg class={format!("w-{} h-{} fill-current text-gray-300 group-hover:text-white transition-colors duration-300", props.svg_size, props.svg_size)} viewBox="0 0 24 24">
+                    <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"/>
+                </svg>
+            </a>
+
             if props.professional {
-
-                // linkedin
-                <a 
-                    href="https://www.linkedin.com/in/konstantinos-rokanas-1ab1a113a/" 
-                    target="_blank" 
-                    class={format!("w-{} h-{} bg-[#1a1a1a] hover:bg-blue-600 rounded-lg flex items-center justify-center transition-all duration-300 hover:scale-110 group border-2 border-black hover:border-blue-500", props.button_size, props.button_size)}
-                    title="LinkedIn"
-                >
-                    <svg class={format!("w-{} h-{} fill-current text-gray-300 group-hover:text-white transition-colors duration-300", props.svg_size, props.svg_size)} viewBox="0 0 24 24">
-                        <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"/>
-                    </svg>
-                </a>
-
                 // cv
                 <button 
                     onclick={download_cv}
@@ -75,7 +74,6 @@ pub fn social_buttons(props: &SocialButtonProps) -> Html {
                 </button>
 
             } else {
-
                 // discord
                 <a 
                     href="https://discord.gg/TODO" 
